661d70a89cb5ec3c4cda992da81be20bd1d6f9e3
tiannannan <tiannannan@nutfin.cn>
2018-03-19 15:10:23 +0800

    init

---
+++ app/src/main/java/com/example/admin/jsbridge/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.admin.jsbridge;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.github.lzyzsd.jsbridge.DefaultHandler;
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TAG = "MainActivity";
+
+    BridgeWebView webView;
+
+    Button button;
+
+    int RESULT_CODE = 0;
+
+    static class Location {
+        String address;
+    }
+
+    static class User {
+        String name;
+        Location location;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+
+        webView.setDefaultHandler(new myHadlerCallBack());
+
+        webView.loadUrl("file:///android_asset/demo.html");
+
+        webView.registerHandler("submitFromWeb", new BridgeHandler() {
+
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                String str = "这是html返回给java的数据:" + data;
+                Toast.makeText(MainActivity.this, "java原生方法被调用了"+str, Toast.LENGTH_LONG).show();
+                //回调返回给Js
+                function.onCallBack("web调用原生");//该参数是传递给html的
+            }
+
+        });
+
+        User user = new User();
+        Location location = new Location();
+        location.address = "SDU";
+        user.location = location;
+        user.name = "大头鬼";
+
+        //不需要回调
+        webView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
+            @Override
+            public void onCallBack(String data) {
+
+            }
+        });
+
+        //java执行完js方法需要回调值时使用
+        webView.addJavascriptInterface(new JavaScriptInterface(), "huidiao");
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsParse("hello world 111");
+            }
+        });
+
+    }
+
+    //java执行完js方法无回传值
+//    public void JsParse(String data){
+//        webView.loadUrl("javascript:dealWithRequest('" + data + "')");
+//    }
+
+    //java执行完js方法有回传值
+    public void JsParse(String data){
+        String url = "javascript:huidiao.startFunction(dealWithRequest('" + data + "'));";
+        webView.loadUrl(url);
+    }
+    private class JavaScriptInterface {
+        @JavascriptInterface
+        public void startFunction(String result) {
+            String aString = result;
+            Toast.makeText(MainActivity.this, aString, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (button.equals(v)) {
+            webView.callHandler("functionInJs", "data from Java", new CallBackFunction() {
+
+                @Override
+                public void onCallBack(String data) {
+                    Log.i(TAG, "reponse data from js " + data);
+                }
+
+            });
+        }
+
+    }
+
+    /**
+     * 自定义回调
+     */
+    class myHadlerCallBack extends DefaultHandler {
+
+        @Override
+        public void handler(String data, CallBackFunction function) {
+            if(function != null){
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+}
